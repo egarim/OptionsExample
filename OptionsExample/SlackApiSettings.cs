83aeeb8474d8645181eb290bc25e0c58f4c0b656
Jose Ojeda <joche.ojeda@bitframeworks.com>
2021-09-25 09:22:49 -0700

    Add project files.

---
+++ OptionsExample/SlackApiSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsExample
+{
+    public class SlackApiSettings
+    {
+        public SlackApiSettings()
+        {
+        }
+        public string WebhookUrl { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
---
+++ OptionsExample/SlackNotificationService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Linq;
+
+namespace OptionsExample
+{
+    public class SlackNotificationService
+    {
+        public SlackNotificationService(IOptionsSnapshot<SlackApiSettings> options)
+        {
+            // fetch the settings for each channel
+            SlackApiSettings devSettings = options.Get("Dev");
+            SlackApiSettings generalSettings = options.Get("General");
+            SlackApiSettings publicSettings = options.Get("Public");
+        }
+    }
+}
